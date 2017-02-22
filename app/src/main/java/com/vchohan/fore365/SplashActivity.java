592f0dbc2e6f05cfc9fdb793bb6d46693b928440
Vineeth Chohan <vineeth.chohan@gmail.com>
2017-02-21 23:36:34 -0500

    Fore365 - 2nd Commit *Adding Splash and Intro Activity

--- app/src/main/java/com/vchohan/fore365/SplashActivity.java
+++ app/src/main/java/com/vchohan/fore365/SplashActivity.java
@@ -1,13 +1,28 @@
 package com.vchohan.fore365;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    //splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_activitty);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashActivity.this, IntroActivity.class);
+                splashIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(splashIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
